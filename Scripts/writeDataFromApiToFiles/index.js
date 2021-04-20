bb36fe618b52848bc196cbd7f345e7abcbee2270
murko <marko.aheienkov@gmail.com>
2021-04-20 14:15:10 +0300

    Add FileSystems class and fetched data from API

---
+++ Scripts/writeDataFromApiToFiles/index.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const fetch = require('node-fetch');
+const {FileSystemWriter, FileSystemReader} = require('../../Classes/FileSystemJSON');
+
+const REQUESTS_PER_DAY = 300;
+
+const ACCESS_KEY = '3e0a35b684d94cb02fb32974d52f0a2d';
+
+const PATH_TO_FILE = path.resolve(__dirname, '..', '..', 'Database', 'db.json');
+
+const getData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
+
+const getDataAndWriteIt = async () => {
+  const dataPromises = [];
+
+  for (let i = 0; i < REQUESTS_PER_DAY; i++) {
+    dataPromises.push(getData(
+        `http://api.aviationstack.com/v1/flights?access_key=${ACCESS_KEY}&limit=100&offset=${i}`,
+    ));
+  }
+
+  const responces = await Promise.all(dataPromises);
+
+  const mapData = responces.map((responce) => responce.data).flat();
+
+  console.log(mapData);
+
+  await FileSystemWriter.write(PATH_TO_FILE, mapData);
+};
+
+getDataAndWriteIt();
+
+// const getDataFromFile = async () => {
+//   const data = await FileSystemReader.read(PATH_TO_FILE);
+//   console.log(data);
+// };
+
+// getDataFromFile();
