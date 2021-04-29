1c5609d4638c52455cdc254c6ff1253870fe44a7
murko <marko.aheienkov@gmail.com>
2021-04-29 21:33:36 +0300

    Add posts routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import articleRoutes from './Routes/articles';
+import postRoutes from './Routes/posts';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.urlencoded());
 
 app.use('/articles', articleRoutes);
+
+app.use('/posts', postRoutes);
 
 app.listen(PORT, () => {
   console.log('Service is running on PORT:', PORT);
